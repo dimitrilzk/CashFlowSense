8b1fe19f5f35e4af19e12ec2afd3ade301d8b2d7
dimitrilzk <dimitrilazuka95@gmail.com>
2025-09-28 12:06:42 +0200

    init routes

--- CashFlowSense.Frontend/src/app/app.routes.ts
+++ CashFlowSense.Frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/components/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+    title: 'Register',
+  },
+];
