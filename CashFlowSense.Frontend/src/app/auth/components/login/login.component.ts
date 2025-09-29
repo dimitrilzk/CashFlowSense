06a7c93278163f6d13550150376b71f876f779b9
dimitrilzk <dimitrilazuka95@gmail.com>
2025-09-29 19:41:18 +0200

    login wip

--- CashFlowSense.Frontend/src/app/auth/components/login/login.component.ts
+++ CashFlowSense.Frontend/src/app/auth/components/login/login.component.ts
@@ -1,9 +1,45 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  TuiAppearance,
+  TuiButton,
+  TuiError,
+  TuiTextfield,
+  TuiTitle,
+  TuiIcon,
+} from '@taiga-ui/core';
+import { TuiFieldErrorPipe } from '@taiga-ui/kit';
+import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    ReactiveFormsModule,
+    TuiAppearance,
+    TuiButton,
+    TuiCardLarge,
+    TuiError,
+    TuiFieldErrorPipe,
+    TuiForm,
+    TuiHeader,
+    TuiTextfield,
+    TuiTitle,
+    TuiIcon,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.less',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected readonly form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl(''),
+    passwordValue: new FormControl(''),
+  });
+}
