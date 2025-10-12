c3f828fcdd64f8de9946a1e1670b2b9e81f8c429
dimitrilzk <dimitrilazuka95@gmail.com>
2025-10-12 18:02:41 +0200

    wip form

--- CashFlowSense.Frontend/src/app/auth/components/register/register.component.ts
+++ CashFlowSense.Frontend/src/app/auth/components/register/register.component.ts
@@ -6,7 +6,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { TuiDay, TuiTime } from '@taiga-ui/cdk';
 import {
   TuiButton,
   TuiIcon,
@@ -53,23 +52,11 @@ import { TuiTextfieldControllerModule } from '@taiga-ui/legacy';
   ],
 })
 export class RegisterComponent {
-  protected testForm = new FormGroup({
-    nameValue: new FormControl('', Validators.required),
-    textValue: new FormControl('', Validators.required),
-    passwordValue: new FormControl('', Validators.required),
-    phoneValue: new FormControl('', Validators.required),
-    moneyValue: new FormControl('100', Validators.required),
-    periodValue: new FormControl(new TuiDay(2017, 2, 15), Validators.required),
-    timeValue: new FormControl(new TuiTime(12, 30), Validators.required),
-    // personValue: new FormControl(this.persons[0]),
-    quantityValue: new FormControl(50_000),
-    radioValue: new FormControl('with-commission'),
-    // accountWherefrom: new FormControl<Account | null>(null),
-    // accountWhere: new FormControl<Account | null>(null),
-    checkboxValue: new FormControl(false),
-    osnoValue: new FormControl(true),
-    usnValue: new FormControl(false),
-    eshnValue: new FormControl(false),
-    envdValue: new FormControl(false),
+  protected form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    lastName: new FormControl(''),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
   });
 }
